449dff34f9de3bbee1be98f4358b716899d0d7d8
devyinka <devyinka8380@gmail.com>
2026-08-23 21:31:40 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   services/authentcationServices.ts 	modified:   services/type.ts

--- services/type.ts
+++ services/type.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface User {
   email: string;
   password: string;
@@ -29,34 +28,3 @@ export interface Course {
   title: string;
   level: number;
 }
-=======
-export interface User {
-  email: string;
-  password: string;
-  role: string;
-  matricNumber: string;
-  phoneNumber: string;
-  fullName: string;
-  department: string;
-  level: string;
-}
-
-export interface Response {
-  id: string;
-  email: string;
-  role: string;
-  matricNumber: string;
-  phoneNumber: string;
-  fullName: string;
-  token: string;
-  imageprofile: string;
-  level: string;
-  department: string;
-}
-
-export interface Course {
-  course_code: string;
-  title: string;
-  level: number;
-}
->>>>>>> f77b8c655103630780787c2ad4f27642707ec977
